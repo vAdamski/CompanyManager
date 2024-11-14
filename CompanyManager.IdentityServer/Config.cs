46bebc0e4d0ff01248ba99b5e2347db9edb44a12
Adam Ludwiczak <64928495+vAdamski@users.noreply.github.com>
2024-11-14 23:50:28 +0100

    Add additional identity resources and configure API client
    Updated the identity resources to include user role, email, first name, and last name claims. Modified the API scopes and client configuration for better alignment with the new identity resources, enhancing support for Swagger integration.

--- CompanyManager.IdentityServer/Config.cs
+++ CompanyManager.IdentityServer/Config.cs
@@ -1,4 +1,5 @@
 ﻿using Duende.IdentityServer.Models;
+using IdentityModel;
 
 namespace CompanyManager.IdentityServer;
 
@@ -9,44 +10,48 @@ public static class Config
 		{
 			new IdentityResources.OpenId(),
 			new IdentityResources.Profile(),
+			new IdentityResource(
+				name: "user",
+				userClaims: new[] { JwtClaimTypes.Email }),
+			new IdentityResource(
+				name: "role",
+				userClaims: new[] { JwtClaimTypes.Role }),
+			new IdentityResource(
+				name: "firstName",
+				userClaims: new[] { "firstName" }),
+			new IdentityResource(
+				name: "lastName",
+				userClaims: new[] { "lastName" }),
 		};
 
 	public static IEnumerable<ApiScope> ApiScopes =>
 		new ApiScope[]
 		{
-			new ApiScope("scope1"),
-			new ApiScope("scope2"),
+			new ApiScope("api1"),
 		};
 
 	public static IEnumerable<Client> Clients =>
 		new Client[]
 		{
-			// m2m client credentials flow client
 			new Client
 			{
-				ClientId = "m2m.client",
-				ClientName = "Client Credentials Client",
+				ClientId = "swagger",
+				ClientName = "Client for Swagger use",
 
-				AllowedGrantTypes = GrantTypes.ClientCredentials,
-				ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-				AllowedScopes = { "scope1" }
-			},
-
-			// interactive client using code flow + pkce
-			new Client
-			{
-				ClientId = "interactive",
-				ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-				AllowedGrantTypes = GrantTypes.Code,
-
-				RedirectUris = { "https://localhost:44300/signin-oidc" },
-				FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-				PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-				AllowOfflineAccess = true,
-				AllowedScopes = { "openid", "profile", "scope2" }
+				AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+				ClientSecrets = { new Secret("secret".Sha256()) },
+				AllowedScopes =
+				{
+					"api1",
+					"openid",
+					"profile"
+				},
+				AlwaysSendClientClaims = true,
+				AlwaysIncludeUserClaimsInIdToken = true,
+				AllowAccessTokensViaBrowser = true,
+				RedirectUris = { "https://localhost:5001/swagger/oauth2-redirect.html" },
+				AllowedCorsOrigins = { "https://localhost:5001" },
+				Enabled = true
 			},
 		};
 }
