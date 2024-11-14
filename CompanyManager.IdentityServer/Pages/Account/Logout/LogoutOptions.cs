8b4cca0bd1c9d3b1065f81963fdd6ad32669217f
Adam Ludwiczak <64928495+vAdamski@users.noreply.github.com>
2024-11-15 00:11:35 +0100

    Deleted odl project

--- CompanyManager.IdentityServer/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace CompanyManager.IdentityServer.Pages.Logout;
-
-public class LogoutOptions
-{
-	public static bool ShowLogoutPrompt = true;
-	public static bool AutomaticRedirectAfterSignOut = false;
-}
