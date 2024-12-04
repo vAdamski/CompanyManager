564ae1a612bf2311cceda0803839c174202938ef
Adam Ludwiczak <64928495+vAdamski@users.noreply.github.com>
2024-12-04 10:30:07 +0100

    Add ServiceBusConsumer project for debugging
    Introduced a new project, CompanyManager.Debug.ServiceBusConsumer, to facilitate debugging with Azure Service Bus. The project includes a basic program to receive messages from the CompanyManager.IdentityServer.CreateUser queue, using the Azure.Messaging.ServiceBus package. This setup allows for easier testing and development using the local development emulator.

---
+++ CompanyManager.Debug.ServiceBusConsumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using Azure.Messaging.ServiceBus;
+
+var connectionString =
+	"Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
+
+// Receive message from the queue CompanyManager.IdentityServer.CreateUser
+await using (var client = new ServiceBusClient(connectionString))
+{
+	ServiceBusReceiver receiver = client.CreateReceiver("CompanyManager.IdentityServer.CreateUser");
+
+	var messages = await receiver.ReceiveMessagesAsync(maxMessages: 10, maxWaitTime: TimeSpan.FromSeconds(5));
+	
+	foreach (ServiceBusReceivedMessage message in messages)
+	{
+		Console.WriteLine($"Received message: {Encoding.UTF8.GetString(message.Body)}");
+		// await receiver.CompleteMessageAsync(message);
+	}
+}
