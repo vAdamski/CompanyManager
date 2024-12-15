9a37a7e9248129d1c07e25b1581fa91f1a4995af
Adam Ludwiczak <64928495+vAdamski@users.noreply.github.com>
2024-12-15 11:52:06 +0100

    Add EmployeeSchools table to track employee education
    Introduced a new `EmployeeSchools` table with necessary fields and relationships in the database. This includes a migration and updates to the DbContext model to support linking employee education details. The new table is integrated with the `Employees` table via a foreign key relationship.

---
+++ CompanyManager.Persistence/Migrations/20241215105135_AddedEmployeeSchoolsTable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyManager.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedEmployeeSchoolsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeSchools",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EmployeeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SchoolName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    YearsIncluded = table.Column<int>(type: "int", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    InactivatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Inactivated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeSchools", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeSchools_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeSchools_EmployeeId",
+                table: "EmployeeSchools",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeSchools");
+        }
+    }
+}
